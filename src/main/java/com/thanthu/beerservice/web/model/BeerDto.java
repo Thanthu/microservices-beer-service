008266a133c99d7ab761e81c349a70b6fbd2c0f6
Thanthu <tnair@orthofx.com>
2021-09-23 20:30:44 +0530

    Create Data Model for Beer Service. Closes #1.

---
+++ src/main/java/com/thanthu/beerservice/web/model/BeerDto.java
@@ -0,0 +1,29 @@
+package com.thanthu.beerservice.web.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto  {
+
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private String upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+}
---
+++ src/main/java/com/thanthu/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.thanthu.beerservice.web.model;
+
+public enum BeerStyleEnum {
+	LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
